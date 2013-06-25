91752809731307f948a5796a004a9364bf2e3240
Georgiy <goshazvir@gmail.com>
2013-06-25 18:37:33 +0300

    load

--- js/common.js
+++ js/common.js
@@ -1,5 +1,21 @@
-$(document).ready(function () {
+// function setEqualHeight(columns)
+// {
+// 	var tallestcolumn = 0;
+// 	columns.each(
+// 	function()
+// 	{
+// 		currentHeight = $(this).height();
+// 		if(currentHeight > tallestcolumn)
+// 		{
+// 			tallestcolumn = currentHeight;
+// 		}
+// 	});
+// 	columns.height(tallestcolumn);
+// }
+// 	$(document).ready(function() {
+// 	setEqualHeight($(".journal-table__item > td"));
+// });
 
-	$('.scroller').baron();
 
-});
+
+
